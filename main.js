efb2d90cec62b37bacee38a662f9bbfc495eada8
Viktoriia Kleiko <vika.kleyko@gmail.com>
2020-06-04 21:49:59 -0700

    lesson 1, added variables money, income, addExpenses, deposit, mission, period; created alert and console.log()

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('hi there');
+var money = 'my money';
+var income = 50000; 
+let addExpenses = true;
+let deposit = 10000;
+const mission = 'my mission'; 
+const period = '1 month';
+
+alert('hi there!');
+console.log('I just started my JS course');
